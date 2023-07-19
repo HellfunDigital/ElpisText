b233cfba5f16001f91f27c10fa5d26681a64dee2
Hellfun Digital <47078754+HellfunDigital@users.noreply.github.com>
2023-07-19 13:33:45 +1000

    Delete root directory

--- root/tools/character_count.py
+++
@@ -1,8 +0,0 @@
-```python
-class CharacterCountTool:
-    def __init__(self):
-        pass
-
-    def count_characters(self, text):
-        return len(text)
-```
--- root/tools/word_count.py
+++
@@ -1,9 +0,0 @@
-```python
-class WordCountTool:
-    def __init__(self):
-        pass
-
-    def count_words(self, text):
-        words = text.split()
-        return len(words)
-```
